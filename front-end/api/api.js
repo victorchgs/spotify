caba50c98c52d2028d19e4551dafeee0f6041fbc
Víctor Chagas <victorchagaslm@gmail.com>
2025-08-06 20:02:52 -0300

    removendo dotenv

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const URL = "http://localhost:3001/api";
-const URL = "https://spotify-nqfn.onrender.com/api";
+const URL = "http://localhost:3001/api";
+// const URL = "https://spotify-nqfn.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
