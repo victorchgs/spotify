03f736bf9bbe20d30e7efc74a3cdfec840ce1121
Víctor Chagas <victorchagaslm@gmail.com>
2025-08-06 19:55:38 -0300

    Quick fix

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,7 @@
-import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+// const URL = "http://localhost:3001/api";
+const URL = "https://spotify-nqfn.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
