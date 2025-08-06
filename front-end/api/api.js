34c638706d2018be7d3cdf6f29114351a2b77bb0
Víctor Chagas <victorchagaslm@gmail.com>
2025-08-06 20:14:33 -0300

    corrigindo api url

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // const { NODE_ENV } = process.env;
 // const URL = "http://localhost:3001/api";
-const URL = "https://deploy-jornada-full-stack.onrender.com/api";
+const URL = "https://spotify-nqfn.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
